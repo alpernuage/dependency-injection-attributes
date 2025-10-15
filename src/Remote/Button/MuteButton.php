1abbd25a9491e6f9471457c1fa393b6ed5225708
alpernuage <alperakbulut@gmail.com>
2025-10-15 11:58:03 +0200

    use Exclude attribute

--- src/Remote/Button/MuteButton.php
+++ src/Remote/Button/MuteButton.php
@@ -3,8 +3,10 @@
 namespace App\Remote\Button;
 
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
 
 #[AsTaggedItem('mute')]
+#[Exclude]
 final class MuteButton implements ButtonInterface
 {
     public function press(): void
